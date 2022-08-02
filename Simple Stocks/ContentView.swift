6fca4925c2a26bb9156cb35d2bf9e12c2e1bd22b
Enric Herce <enric.herce@cuatroochenta.com>
2022-08-02 16:38:22 +0200

    Initial Commit

---
+++ Simple Stocks/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Simple Stocks
+//
+//  Created by Enric Herce on 2/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Simple Stocks/Simple_StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_StocksApp.swift
+//  Simple Stocks
+//
+//  Created by Enric Herce on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Simple_StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
